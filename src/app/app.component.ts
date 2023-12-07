4f12fa1465a65e36bdd00e4de1fb698c9051bcae
harry3574 <harry357.gaming@gmail.com>
2023-12-07 14:24:05 +0100

    removed some uneeded stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,10 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { MoneModule } from './mone/mone.module';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, MoneModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']  // Use styleUrls instead of styleUrl
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'inventory-labo';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
 import { UserFormComponent } from './mone/user-form/user-form.component';
+
 
 @NgModule({
   declarations: [
