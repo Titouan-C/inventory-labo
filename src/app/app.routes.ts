b066e1aa24b70e73afa51d0c9d456340124b8c09
Titouan-C <titouan.chabanon31@gmail.com>
2023-12-07 13:49:58 +0100

    add navbar component, begin routes file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './mone/navbar/navbar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home/', component: NavbarComponent },
+    { path: '', redirectTo: 'home/', pathMatch: 'full' }
+];
