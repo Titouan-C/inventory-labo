468043a26ba19da4d6aad8465806829549335afc
harry3574 <harry357.gaming@gmail.com>
2023-12-07 13:47:25 +0100

    pretty much everything
    still need to fix a few things

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserFormComponent } from './mone/user-form/user-form.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule // Add the AppRoutingModule here
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
